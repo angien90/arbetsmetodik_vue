864511feb8f458fa88a9a379cf31562726b3b652
angien90 <angelicanylander@hotmail.com>
2025-02-15 14:05:04 +0100

    fix(change base): change base again to repo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/https://github.com/angien90/arbetsmetodik_vue/',
+  base: '/angien90/arbetsmetodik_vue/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
